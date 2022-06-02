83e22631a01b6ca4c879b39e5035c1f9de57d03b
Rahat <mdrahatulislam121@.com>
2022-06-02 15:38:00 +0600

    third commit

--- writeFile.js
+++ writeFile.js
@@ -1,20 +1,41 @@
+// const fs= require('fs')
+
+
+// const testObj={
+//     name:'Rahat',
+//     email:'islamrahatul092@gmail.com',
+//     address:{
+//         city:'Rangpur',
+//         country:'Bangladesh'
+//     }
+// }
+
+// const data=JSON.stringify(testObj)
+// fs.writeFile('./test.json', data,(err)=>{
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log('File write successfully');
+//     }
+// })
+
+
 const fs= require('fs')
 
-
 const testObj={
-    name:'Rahat',
-    email:'islamrahatul092@gmail.com',
+    name: 'MR Rahatul Islam',
+    email: 'islamrahatul092@gnail.com',
     address:{
         city:'Rangpur',
-        country:'Bangladesh'
+        country:"Bangladsh"
     }
 }
 
 const data=JSON.stringify(testObj)
-fs.writeFile('./test.json', data,(err)=>{
-    if (err) {
+fs.writeFile('./test.json',data,(err)=>{
+    if(err){
         console.log(err);
-    }else{
-        console.log('File write successfully');
+    } else{
+        console.log('file write succesfully');
     }
 })
