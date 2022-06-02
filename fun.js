95c9fb816d65d819eb06a546b39aad3704cb0295
Rahat <mdrahatulislam121@.com>
2022-06-02 16:57:43 +0600

    6th commit

--- fun.js
+++ fun.js
@@ -1,10 +1,21 @@
+// const fs = require('fs')
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     fs.readFile('./index.html', (err, data) => {
+//         res.write(data)
+//         res.end()
+//     })
+// })
+
+// server.listen(8080, () => console.log('Server Is Running'))
+
+
 const fs=require('fs')
 const http=require('http')
-const server = http.createServer((req,res)=>{
-    fs.readFile('./index.html', (err,data)=>{
+const server =http.createServer((req,res)=>{
+    fs.readFile('./index.html',(err,data)=>{
         res.write(data)
         res.end()
     })
 })
-
-server.listen(8080,()=> console.log('Server Is Running'))
+server.listen(3024, ()=> console.log('Server Is Running Running'))
