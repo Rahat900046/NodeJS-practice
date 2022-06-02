df3baf9b4b774d107e836d4a14756fa050f53c79
Rahat <mdrahatulislam121@.com>
2022-06-02 16:04:27 +0600

    fifth commit

--- http.js
+++ http.js
@@ -1,8 +1,19 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//     console.log(req.url);
+//     res.end('<h1>Hello NodeJS.Yor are my Love</h1>')
+// })
+// server.listen(3000, () => {
+//     console.log('Server Is Running pn PORT 3000');
+// })
+
+
+
+const http=require('http')
+const server=http.createServer((req,res)=>{
     console.log(req.url);
-    res.end('<h1>Hello NodeJS.Yor are my Love</h1>')
+    res.end('<h1>Hello Node JS.You  are Beauty</h1>')
 })
-server.listen(3000, () => {
-    console.log('Server Is Running pn PORT 3000');
+server.listen(3024,()=>{
+    console.log('Server is Running ON PORT 3024');
 })
